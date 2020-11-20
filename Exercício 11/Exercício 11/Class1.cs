8503b5926c13ccd837d545fed52665a5d9752aac
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:30:17 -0300

    Add files via upload

---
+++ Exercício 11/Exercício 11/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_11
+{
+    class Pessoa
+    {
+        double peso;
+        double altura;
+        double r;
+
+        public void SetPeso(double p)
+        {
+            peso = p;
+        }
+
+        public void SetAltura(double p)
+        {
+            altura = p;
+        }
+
+        public double GetPeso()
+        {
+            return peso;
+        }
+
+        public double GetAltura()
+        {
+            return altura;
+        }
+
+        public double GetR()
+        {
+            return r;
+        }
+
+        public double CalcularR()
+        {
+            r = peso / Math.Pow(altura,2);
+            return r;
+        }
+
+        public string ExibirMsg()
+        {
+            string k;
+
+            if (CalcularR() < 20) k = "Abaixo do peso";
+            else if (CalcularR() >= 25) k = "Acima do peso";
+            else k = "Peso ideal";
+
+            return k;
+        }
+    }
+}
---
+++ Exercício 11/Exercício 11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercício_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Pessoa();
+            double k;
+
+            do
+            {
+                Console.Write("Informe o peso da pessoa em quilogramas: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetPeso(k);
+            }
+            while (k <= 0);
+
+            do
+            {
+                Console.Write("Informe a altura da pessoa em metros: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetAltura(k);
+            }
+            while (k <= 0);
+
+            Console.WriteLine($"Esta pessoa está com status de {a1.ExibirMsg()}");
+        }
+    }
+}
